5fe10f5c158de24847e29dbbc3056353463f41d5
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-07-18 21:09:53 +0530

    Add FE: Package axios , react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import MatchPage from './pages/MatchPage';
+// import VideoChatPage from './pages/VideoChatPage';
 
-const App = () => {
-  
+function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/match" element={<MatchPage/>} />
+        {/* <Route path="/video/:roomId" element={<VideoChatPage/>} /> */}
+      </Routes>
+    </BrowserRouter>
   );
-};
-
+}
 export default App;
